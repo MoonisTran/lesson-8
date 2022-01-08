7fc5b0bbea95daf4021a1abe5ce44cb628e95456
MoonisTran <hoangnhu715@gmail.com>
2022-01-08 12:47:55 +0700

    1st

---
+++ lesson 8/script.js
@@ -0,0 +1,7 @@
+let phepTinh = prompt ("Chọn 1 trong 4 phép tính sau cho a và b: + - * /")
+let a = prompt("Nhập số a:")
+    let b = prompt ("Nhập số b:")
+if (phepTinh = "+" ) {
+    
+    console.log("Kết quả:", parseIn(a) + parseIn(b));
+}
